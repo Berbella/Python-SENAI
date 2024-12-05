64865b5a3474390c1e391d4675c1bc592a636c10
Isabella da Silva Carvalho <isabelladasilvacarvalho04@gmail.com>
2024-12-05 09:57:41 -0300

    Add files via upload

---
+++ Aula 2/aula02.py
@@ -0,0 +1,11 @@
+print('Olá, bom dia!')
+
+nome_usuario = 'Isabella Carvalho' # tipo -> str = string
+idade_usuariio = 19 # tipo -> int = integer
+altura_usuario = 1.56 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print ('Seja Bem-vindo', nome_usuario)
+print ('Sua idade é', idade_usuariio, 'anos,')
+print ('Você tem', altura_usuario, "metros.")
+print ('Você é maior de idade?', maior_idade)
